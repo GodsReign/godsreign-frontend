e66a17965a9b254e10a3d7127a7bfe8c37dea431
Md Aftab <mda552669@gmail.com>
2022-11-08 22:17:34 +0530

    added hover effect on partner logo and products

--- src/components/dashboard/partner.jsx
+++ src/components/dashboard/partner.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import partnerLogo from "../../assets/partner_logo.svg";
+import partnerLogoHover from "../../assets/partner_logo_gold.svg"
 function Partner() {
+  const [logo, setLogo] = useState(partnerLogo);
+  const handleMouseOver = (e) => {
+    setLogo(partnerLogoHover);
+    e.target.setAttribute('style', 'transform:scale(1.05)');
+  };
+  const handleMouseOut = (e) => {
+    setLogo(partnerLogo)
+
+    e.target.setAttribute("style", "transform:scale(1.0)");
+
+  }
   return (
     <div className="tw-flex tw-items-center tw-mt-24">
       <h3 className="partner__heading">Official Commercial Partner</h3>
-      <img src={partnerLogo} alt="DNA_logo" />
+      <img onMouseOver={(e) => handleMouseOver(e)} onMouseOut={(e)=>handleMouseOut(e)} src={logo} alt="DNA_logo" className="partner__logo" />
     </div>
   );
 }
