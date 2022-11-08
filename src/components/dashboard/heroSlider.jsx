0ebc21a1dd2b42e0a546cc2434de3c708589214c
Md Aftab <mda552669@gmail.com>
2022-11-08 19:20:19 +0530

    starting animation

--- src/components/dashboard/heroSlider.jsx
+++ src/components/dashboard/heroSlider.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import videoUrls from "./videoUrls";
+import { motion } from "framer-motion";
 function HeroSlider() {
   const [curIndex, setCurIndex] = useState(0);
+  const [delayAni, setDelayAni] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setDelayAni(1.5);
+      setCurIndex((curIndex) => (curIndex < 2 ? curIndex + 1 : 0));
+    }, 5000);
 
-  // useEffect(() => {
-  //   setTimeout(
-  //     () => setCurIndex((curIndex) => (curIndex < 2 ? curIndex + 1 : 0)),
-  //     30000
-  //   );
-
-  //   // console.log(curIndex);
-  // });
+    // console.log(curIndex);
+  });
   const handleClick = (index) => {
     setCurIndex(index);
   };
-  console.log(videoUrls);
+  //console.log(videoUrls);
   return (
     <div className="container tw-flex tw-flex-col tw-text-white slider">
-      <section className="tw-flex tw-items-center slider ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.5, duration: 1.5 }}
+        className="tw-flex tw-items-center slider "
+      >
         <header>
           <button className="btn token token--greyscale">
             {videoUrls[curIndex].game}
           </button>
-          <section className="tw-pb-8 tw-pt-8">
+          <section className="tw-pb-8 tw-pt-8 ">
             <h2 className="carousel__heading">{videoUrls[curIndex].heading}</h2>
             <p className="carousel__content">{videoUrls[curIndex].content}</p>
           </section>
@@ -44,7 +50,7 @@ function HeroSlider() {
             alt=""
           />
         </span>
-      </section>
+      </motion.div>
       <div className="tw-flex tw-pt-8">
         {videoUrls.map((item, index) => {
           return (
