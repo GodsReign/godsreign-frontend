90b29f2176ac967a6b90a29eb9ca34994d716dac
Md Aftab <mda552669@gmail.com>
2022-11-11 04:00:27 +0530

    livebar and navabar sticky and responsive

--- src/components/playground/playground.jsx
+++ src/components/playground/playground.jsx
@@ -1,7 +1,57 @@
 import React from "react";
-import "../../components/LiveBar/LiveBar.css";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+// Default theme
+import "@splidejs/react-splide/css";
+// or other themes
+import "@splidejs/react-splide/css/skyblue";
+import "@splidejs/react-splide/css/sea-green";
+
+// or only core styles
+import "@splidejs/react-splide/css/core";
+import "./playground.css";
+import utilSLide from "./utliSLide";
+import CustomVideoPlayer from "../videoPlayer/CustomVideoPlayer";
+import LiveBarModal from "../LiveBar/LiveBarModal";
+
 const Playground = () => {
-  return <></>;
+  return (
+    // <section className="top_slider">
+    //   <Splide
+    //     hasTrack={false}
+    //     options={{
+    //       rewind: true,
+    //       gap: "1rem",
+    //       perPage: 2,
+    //     }}
+    //     className="section"
+    //     aria-label="My Favorite Images"
+    //   >
+    //     <SplideTrack>
+    //       {utilSLide.map(({ content }, index) => {
+    //         return (
+    //           <SplideSlide key={index}>
+    //             <section className="content_slide">
+    //               <span>{content}</span>
+    //             </section>
+    //           </SplideSlide>
+    //         );
+    //       })}
+    //     </SplideTrack>
+    //     <button className="splide__toggle" type="button">
+    //       <span className="splide__toggle__play">Play</span>
+    //       <span className="splide__toggle__pause">Pause</span>
+    //     </button>
+    //   </Splide>
+    // </section>
+    <div>
+      <div className="playground container tw-relative">
+        
+      </div>
+      <div>
+        <LiveBarModal />
+      </div>
+    </div>
+  );
 };
 
 export default Playground;
---
+++ src/components/playground/utliSLide.js
@@ -0,0 +1,25 @@
+const utilSLide = [
+  {
+    id: 0,
+    content:
+      "enkdjnejknewkjfnewkjfdskdaskdjasdkasjdsaldksjdasldjasdklsdjsakdjadklsjdaskldjskdjasdklsajdlskdjskldskcnksdaslkdaskldnaslkdnsakd",
+  },
+  {
+    id: 1,
+    content:
+      "enkdjnejknewkjfnewkjfdskdaskdjasdkasjdsaldksjdasldjasdklsdjsakdjadklsjdaskldjskdjasdklsajdlskdjskldskcnksdaslkdaskldnaslkdnsakd",
+  },
+  {
+    id: 2,
+    content:
+      "enkdjnejknewkjfnewkjfdskdaskdjasdkasjdsaldksjdasldjasdklsdjsakdjadklsjdaskldjskdjasdklsajdlskdjskldskcnksdaslkdaskldnaslkdnsakd",
+  },
+  {
+    id: 3,
+    content:
+      "enkdjnejknewkjfnewkjfdskdaskdjasdkasjdsaldksjdasldjasdklsdjsakdjadklsjdaskldjskdjasdklsajdlskdjskldskcnksdaslkdaskldnaslkdnsakd",
+  },
+];
+
+
+export default utilSLide;
